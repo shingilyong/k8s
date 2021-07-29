6837cccf00ca2cc14c6d85c9f385fd02af27fb62
shingilyong <sgy12303@gmail.com>
2021-07-29 12:01:13 +0000

    test

--- test/app.java
+++ test/app.java
@@ -11,7 +11,7 @@ public class DockerApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "H1ello Docker World";
+        return "Hello Docker World";
     }
 
     public static void main(String[] args) {
